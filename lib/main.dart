bda8757e9db85b465540c35b1e6de00354f79a38
Madhur Jain <42210565+madhurDroiddev@users.noreply.github.com>
2025-08-05 23:12:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -34,10 +34,10 @@ class PortfolioApp extends StatelessWidget {
               minWidth: 480,
               defaultScale: true,
               breakpoints: [
-                const ResponsiveBreakpoint.resize(480, name: MOBILE),
-                const ResponsiveBreakpoint.resize(800, name: TABLET),
-                const ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-                const ResponsiveBreakpoint.autoScale(1200, name: "4K"),
+                const Breakpoint(start: 0, end: 450, name: MOBILE),
+                const Breakpoint(start: 451, end: 800, name: TABLET),
+                const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+                const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
               ],
             ),
             theme: ThemeData(
